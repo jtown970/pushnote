553b7b9c9187c90b454e3ea1a7fccee2d656b77a
jtown970 <townertc@gmail.com>
2020-03-30 13:51:40 -0700

    fixed sw

--- push.js
+++ push.js
@@ -7,5 +7,5 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto:test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {};
+  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/eOEgFHHv1-E:APA91bFV_fG4WIJtJgS_GNMN7zxjD0XAXqHIdnH4Cr3YFmx8g4vxkfThxwAbfCSezk_u1sFqHZiinX1MsBzZBOtw7LJ9zv2xnbVzcjK3R80xGnhG5QNpS1DT99i1_GpY3PN5tPmGs9NJ","expirationTime":null,"keys":{"p256dh":"BEGMdeVv2fmcJJlvowe_w7eN56t9giXRO41a_E5ETkylyiN04lL-_ZqPVE2uNh1s1IpvUafEapVjfdRmHTgUiTE","auth":"zI5u_tsH6kkhq2qwV91iMA"}}
   push.sendNotification(sub, 'test message')
